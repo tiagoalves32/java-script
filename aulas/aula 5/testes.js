bb1657f91fac5af179d1479a8b3a929b41d9337f
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-05-15 18:59:28 -0300

    Atualizei o exercício

---
+++ aulas/aula 5/testes.js
@@ -0,0 +1,10 @@
+let nome; // Declarou a variável
+nome = 'João'; // inicializando a variável
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+
+let nomeCliente = 'Luiz';
+let nomecliente = 'Otávio';
+
+console.log(nomeCliente, nomecliente);
