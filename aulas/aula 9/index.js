919991f634fa2e2f727d75a08f1cbd11bee21d97
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-05-17 12:13:45 -0300

    Criei o index.js

---
+++ aulas/aula 9/index.js
@@ -0,0 +1,26 @@
+/**Aritiméticos
+ *  + Adição / Concatenação
+ * -
+ *  * 
+ * /
+ * Potenciação / **
+ * Resto da divisão / %
+ * 
+ *Ordem de precedência dos operadores aritiméticos
+ ()
+  **
+   * * / %
+   *  + - 
+ */
+
+  // Operadores de atribuição -> +=, -=, *=, **= 
+
+// Conversão de dados 
+
+const num1 = 20;
+const num2 = Number('7'); /* A função Number faz a conversão tanto de número inteiro quanto de número decimal.
+*/
+console.log(num1 + num2);
+
+
+
