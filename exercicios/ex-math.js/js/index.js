13a7425fbad5dae8d904121cf5bfb1d93b68c69b
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-05-22 17:43:55 -0300

    Atualizei o index.js

--- exercicios/ex-math.js/js/index.js
+++ exercicios/ex-math.js/js/index.js
@@ -3,6 +3,7 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 
+/*
 texto.innerHTML = '';
 texto.innerHTML = `<p> ${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
 texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
@@ -10,6 +11,7 @@ texto.innerHTML +=`<p>É NaN:${Number.isNaN(numero)}</p> `;
 texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
 texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
 texto.innerHTML += `<p>Com duas casas decimais ${Number(numero.toFixed(2))}</p>`;
+*/
 
 
 
