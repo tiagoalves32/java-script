3cd1ffd81c64ccce4c31fc6595462c057773c799
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-05-25 13:50:04 -0300

    Criei index.js

---
+++ aulas/aula 18/js/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) não se usa 
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) não se usa
+!== diferente estrito (valor e tipo)
+
+const num1 = 10;
+const num2 = 10;
+const comp = num1 !== num2;
+
+console.log(comp);
+*/
