2eec61ea46daf71b9332c7eaeaf7488d18ebdc79
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-05-26 18:20:29 -0300

    Criei o index.js

---
+++ aulas/aula 20/js/index.js
@@ -0,0 +1,38 @@
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true || false -> vai retornar "vai retornar o primeiro valor verdadeiro"
+
+FALSY
+*false(valor literal)
+0
+'' "" ``
+null / undefined NaN
+
+*/
+ /*
+function falaOi () {
+    return 'Oi';
+}
+
+const vaiExecutar = 'Joãozinho';
+
+console.log(vaiExecutar && falaOi());
+*/
+
+/*
+console.log(0 || false || null || 'Luiz Otávio' || true);
+*/
+
+/*
+const corUsuario = null;
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);
+*/
+/*
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+*/
