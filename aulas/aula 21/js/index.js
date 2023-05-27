6366fb4ecd41f710995e6cddd8a2594e5f7e2647
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-05-27 14:50:39 -0300

    Criei o index.js

---
+++ aulas/aula 21/js/index.js
@@ -0,0 +1,90 @@
+/*
+const hora = 13;
+
+if (hora <= 12) {
+    console.log('Bom dia');
+}
+*/
+
+/*
+Entre 0-11 - Bom Dia
+Entre 12 - 17 - Boa Tarde
+Entre 18 - 23 - Boa Noite
+*/
+// If pode ser usado sozinho
+// Sempre que ultilizo a palavra else, preciso de um if antes.
+// Eu posso ter vários else ifs na checagem.
+
+/*
+const hora = 19;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia');  
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa Tarde');
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa Noite');
+}
+*/
+
+// Só posso ter um else na checagem.
+// Podemos usar condições sem else if, ultilizando apenas if e else.
+
+/*
+const tenhoGrana = null;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Não vou sair de casa');
+}
+*/
+
+// Parte 2 
+
+/*
+const numero = 3;
+
+if (numero >= 0  && numero <= 5) {
+    console.log('O número está entre 0 e 5.');
+
+}
+*/
+
+/* 
+SE (numero >= 0 && numero <= 5) ocorrer, faça isso{código}
+SE NÃO faça isso {o código} 
+const numero = 5;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5.');
+} else {
+    console.log('O número NÃO está entre 0 e 5.');
+}
+*/
+
+ 
+  
+/*
+const numero = 10;
+if (numero <= 10) {
+    console.log('O número é menor ou igual a 10');
+}  
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O número está entre 6 e 8.');
+} else if (numero >= 9 && numero <= 11) {
+    console.log('O número está entre 9 e 11.');
+} else {
+    console.log('O número não está entre 0 e 11.');
+}
+*/
+
+
+
+
+ 
+
+
