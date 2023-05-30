abae9c24e0cb98d5f4666ae4ae80ea0550804081
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-05-30 13:18:47 -0300

    Criei o exercício switch

---
+++ exercicios/ex-switch-case/switch-case.js/assets/js/main.js
@@ -0,0 +1,39 @@
+
+/*
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+
+function getDiaSemanaTexto(diaSemana) {
+    const diasSemana = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 
+    'sexta-feira', 'sábado'];
+    return diasSemana[diaSemana];
+}
+        
+    function getNomeMes(numeroMes) {
+        const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto',
+        'setembro', 'outubro', 'novembro', 'dezembro'];
+        return meses[numeroMes]; 
+        }
+
+        function criaData(data) {
+            const diaSemana = data.getDay();
+            const numeroMes = data.getMonth();
+
+            const nomeDia = getDiaSemanaTexto(diaSemana);
+            const nomeMes = getNomeMes(numeroMes); 
+
+            return (`${nomeDia}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}`);
+
+        }
+    
+    h1.innerHTML = criaData(data); 
+    */
+    
+
+    /*
+    const h1 = document.querySelector('.container h1');
+    const data = new Date();
+    h1.innerHTML = data.toLocaleDateString('pt-br', { dataStyle: 'full', timeStyle: 'short' });
+    */
+    
+
