6814a40dc6379243f2e766401b983c03daf4c125
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-06-14 18:32:16 -0300

    criei  aula 27

---
+++ aulas/aula 26/index.js
@@ -0,0 +1,44 @@
+
+
+//const verdadeira = true
+
+/*
+let nome  = 'Luiz'
+var nome2 = 'Luiz'
+
+
+
+if (verdadeira) {
+    let nome = 'Otávio'
+    var nome2 = 'Rogério'
+    
+    // console.log(nome, nome2)
+
+    if (verdadeira) {
+        let nome = ('Outra coisa')
+        var nome2 = 'Ronaldo'
+        
+    } 
+}
+
+console.log(nome, nome2)
+*/
+
+/*
+function falaOi() {
+    
+    if (verdadeira) {
+        let nome = 'Luiz'
+        var sobrenome = 'Miranda'
+          
+    }
+    console.log(sobrenome)     
+}
+falaOi()
+*/
+//let sobrenome = 'Miranda'
+//console.log(sobrenome)
+
+
+
+ 
---
+++ aulas/aula 27/index.js
@@ -0,0 +1,19 @@
+//               0  1  2  3  4  5  6  7  8
+// ... rest, ... spread
+/*
+const numeros = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+const [um, , tres, , cinco, , sete] = numeros
+
+console.log(um, tres, cinco, sete)
+*/
+//                     0         1         2
+//                  0  1  2   0  1  2   0  1  2
+ // const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+
+//  const [,, [,,nove]] = numeros
+ // console.log(nove)
+
+ // const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+ // const [lista1, lista2, lista3] = numeros
+
+ // console.log(lista3[2])
