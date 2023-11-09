dcf8268c1982ecde350b3fab9092a06b6eb7465b
tiagoalves32 <tiagoandradealves04@gmail.com>
2023-11-08 21:04:17 -0300

    Atualizei app.js

--- aulas/aula 1/app.js
+++ aulas/aula 1/app.js
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+console.log('Tiago Andrade'); // String = Texto
+console.log("Tiago Andrade");
+console.log(`Tiago Andrade`);
+console.log("Tiago 'Andrade'");
+console.log('Tiago "Andrade"');
+console.log(`'Tiago' "Andrade"`);
+
+console.log(123456, 15.85); // Number = Número
+console.log(35, 15.85, 'Tiago Andrade de novo');
